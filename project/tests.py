fa6d46cafb955019007eb95d693cabe4a46d54e6
areenabucaf <areenab@ac.sce.ac.il>
2022-04-27 13:59:20 +0300

    Update tests.py

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def 1add2equal3(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+if _name_ == '_main_':
+    unittest.main()
