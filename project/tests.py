067eef92b8688c8ff9c72b7f23d8874be84da746
lyaleaz <74859322+lyaleaz@users.noreply.github.com>
2022-04-27 16:13:06 +0300

    Update tests.py

--- project/tests.py
+++ project/tests.py
@@ -4,25 +4,10 @@ from .models import Driver
 import django
 from django.test import TestCase, tag
 from django import setup
-
-
-class LogTest(TestCase):
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            super(LogTest, cls).setUpClass()
-            django.setup()
-
-    @tag('unit-test')
-    def test_login(self):
-        login=self.client.login(username='soso', password='S263safa')
-        self.assertFalse(login)
-        
-'''
+  
 class LogoutTest(TestCase):
    def testLogout(self):
-       User.objects.create(username='israa1', password='123')
+       User.objects.create(username='lyale1', password='123')
        self.client.login(username='username',password='password')
 
        response = self.client.get(reverse('logout'), follow=True)
@@ -36,4 +21,4 @@ class ManageUsersTest(TestCase):
                                         last_name='last_name',
                                         first_name='first_name')
         self.user.set_password('password')
-        self.user.save()'''
+        self.user.save()
