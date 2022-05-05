7de8d2840e08f2cfde4ca6e30ff4ee663e6bdb5a
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-05 22:37:32 +0300

    continuation

--- project/static/project/test.js
+++ project/static/project/test.js
@@ -1,4 +1,10 @@
+var date=new Date(new Date().getTime() + (3600 * 1000 *3));
+
+
 function initMap() {
+var deptime=document.querySelector('input[type="datetime-local"]').value;
+console.log(date.toISOString().split('.')[0]);
+document.querySelector('input[type="datetime-local"]').value=date.toISOString().split('.')[0];
   const directionsRenderer = new google.maps.DirectionsRenderer();
   const directionsService = new google.maps.DirectionsService();
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -9,15 +15,23 @@ function initMap() {
   directionsRenderer.setMap(map);
   calculateAndDisplayRoute(directionsService, directionsRenderer);
 }
+
 function updatefunc(){
   var deptime=document.querySelector('input[type="datetime-local"]').value;
   date=new Date(deptime);
   console.log(deptime)
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 37.77, lng: -122.447 },
+  });
+
+  directionsRenderer.setMap(map);
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  date=new Date('May 5, 2022 19:01:00');
-  console.log(date.getTime()/1000)
   origin=document.getElementById("frfr").value.toString();
   dest=document.getElementById("toto").value.toString();
   console.log(origin)
