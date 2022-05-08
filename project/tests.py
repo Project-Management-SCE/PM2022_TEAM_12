d285268d3e7c2e66a128f8160d94a0f09b7622b9
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-08 13:49:26 +0300

    unit-test
    unit-test

--- project/tests.py
+++ project/tests.py
@@ -2,7 +2,16 @@
 from django.test import TestCase, tag
 from django.urls import reverse
 from project.models import *
+##################################################################
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.test import TestCase,tag
+from django.urls import reverse
+from django.test import Client
+from App.models import *
+import requests
   
+  # Create your tests here.
 class LogoutTest(TestCase):
    def testLogout(self):
        User.objects.create(username='areen1', password='123123')
@@ -18,5 +27,35 @@ class ManageUsersTest(TestCase):
                                         first_name='first_name')
         self.user.set_password('password')
         self.user.save()
+########################new test#####################
 
-        
+
+
+
+
+
+# ------------tests for some admin functionality  ------     -- ------------------
+#@tag("unit_test")
+class AdminMessageFormTests(TestCase):
+    @tag('unit-test')
+    def test_Add_Message_GET(self):
+        c = Client()
+        response = c.get(reverse('createmessage'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin_templates/admin_message_form.html')
+
+    @tag('unit-test')
+    def test_Add_Message_GET2(self):
+        c = Client()
+        response = c.get(reverse('createmessage'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateNotUsed(response, 'teacher_templates/message_form.html')
+
+    @tag('unit-test')
+    def test_Add_Teacher_Message_GET(self):
+        c = Client()
+        response = c.get(reverse('create_teacher_message'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teacher_templates/message_form.html')
+
+
