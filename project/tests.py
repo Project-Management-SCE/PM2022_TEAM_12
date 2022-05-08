1caa2d26400cd67fa6089dbd5c7b8d2cfb7d2f9a
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-08 14:40:58 +0300

    neww

--- project/tests.py
+++ project/tests.py
@@ -36,13 +36,35 @@ class ManageUsersTest(TestCase):
 
 # ------------tests for some admin functionality  ------     -- ------------------
 #@tag("unit_test")
+<<<<<<< HEAD
 class PagehomeTests(TestCase):
+=======
+class pagehoameTests(TestCase):
+>>>>>>> 22eaf8fd2c841a9dc5029141351557b57a3181f0
     @tag('unit-test')
     def test_ passegers_register(self):
         c = Client()
         response = c.get(reverse('index'))
         self.assertEquals(response.status_code, 200)
+<<<<<<< HEAD
         self.assertTemplateUsed(response, 'signupasdesfaef')
 
+=======
+        self.assertTemplateUsed(response, 'signup')
+
+  '''  @tag('unit-test')
+    def test_Add_Message_GET2(self):
+        c = Client()
+        response = c.get(reverse('createmessage'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateNotUsed(response, 'teacher_templates/message_form.html')
+
+    @tag('unit-test')
+    def test_Add_Teacher_Message_GET(self):
+        c = Client()
+        response = c.get(reverse('create_teacher_message'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teacher_templates/message_form.html')
+>>>>>>> 22eaf8fd2c841a9dc5029141351557b57a3181f0
 
 
