43a3f5124311eee0783bcc307353d54e74397486
areenabucaf <areenab@ac.sce.ac.il>
2022-05-26 03:07:39 +0300

    BSPM2022T12-47

--- project/models.py
+++ project/models.py
@@ -12,29 +12,24 @@ class Driver(User):
     is_ok= models.BooleanField(default=False)
     License=models.FileField(upload_to='files/', null=True)
     certificate=models.FileField(default=None)
-
     class Meta:
         db_table = 'Drivers'
-
 class Updates(models.Model):
     senderID=models.CharField(max_length=100,default='')
     BusLine=models.CharField(max_length=100,default='')
     message=models.CharField(max_length=100,default='')
     class Meta:
         db_table = 'Updates'
-
 class Report(models.Model):
     UserName=models.CharField(max_length=100,default='')
     class Meta:
         db_table = 'Reports'
-
 class Trip(models.Model):
     username=models.CharField(max_length=100,default='')
     To=models.CharField(max_length=100,default='')
     From=models.CharField(max_length=100,default='')
     BusLine=models.CharField(max_length=100,default='')
     DateTime=models.DateTimeField()
-
     class Meta:
         db_table = 'Trips'
 
