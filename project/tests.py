b2ee3afa6f156b08a274395ae8ae60461703fc28
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-29 18:30:10 +0300

    test_PassengerNotification_ToLogin
    test_PassengerNotification_ToLogin

--- project/tests.py
+++ project/tests.py
@@ -587,7 +587,24 @@ class testPassengerHomePage_integration_test_class(TestCase):
 
       response = self.client.get(('logoutUser'), follow=True)#log out
 
-      self.assertNotEqual(response.status_code, 300)                                                                
+      self.assertNotEqual(response.status_code, 300)   
+
+
+
+
+   def test_PassengerNotification_ToLogin(self):
+     response = self.client.get('PassengerNotification')#url שלב 1
+     self.assertTrue(User.is_authenticated)
+
+     response = self.client.get(('Login'))#url אחרי הפעולה
+     self.assertNotEqual(response.status_code, 300)
+
+     response = self.client.get(('logoutUser'), follow=True)#log out
+
+     self.assertNotEqual(response.status_code, 300)              
+
+      
+                                                               
     
          
     
