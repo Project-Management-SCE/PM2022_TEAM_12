40b0164d2ea20768b4efb361a4c5d168afeade2e
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-29 18:22:08 +0300

    add new test
    add new test

--- project/tests.py
+++ project/tests.py
@@ -527,8 +527,17 @@ class testPassengerHomePage_integration_test_class(TestCase):
 
       self.assertNotEqual(response.status_code, 300)  
 
-      
-                                                    
+
+   def test_PassengerHomePage_ToPMyTrip(self):
+      response = self.client.get('PassengerHomePage')#url שלב 1
+      self.assertTrue(User.is_authenticated)
+
+      response = self.client.get(('PMyTrip'))#url אחרי הפעולה
+      self.assertNotEqual(response.status_code, 300)
+
+      response = self.client.get(('logoutUser'), follow=True)#log out
+
+      self.assertNotEqual(response.status_code, 300)                                                   
     
          
     
