2834f774a3b8948772f02039f03ecf08a70fa27e
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-29 18:22:55 +0300

    aa
    aa

--- project/tests.py
+++ project/tests.py
@@ -537,7 +537,19 @@ class testPassengerHomePage_integration_test_class(TestCase):
 
       response = self.client.get(('logoutUser'), follow=True)#log out
 
-      self.assertNotEqual(response.status_code, 300)                                                   
+      self.assertNotEqual(response.status_code, 300)   
+
+
+   def test_PassengerHomePage_PassengerPassword(self):
+      response = self.client.get('PassengerHomePage')#url שלב 1
+      self.assertTrue(User.is_authenticated)
+
+      response = self.client.get(('PassengerPassword'))#url אחרי הפעולה
+      self.assertNotEqual(response.status_code, 300)
+
+      response = self.client.get(('logoutUser'), follow=True)#log out
+
+      self.assertNotEqual(response.status_code, 300)                                                       
     
          
     
