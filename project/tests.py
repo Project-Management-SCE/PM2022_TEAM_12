0f7a7735b2d7aa1ab02a52b4cd93d94f78b5a4fc
hosammk <74871580+hosammk@users.noreply.github.com>
2022-05-29 18:49:11 +0300

    another update
    another update

--- project/tests.py
+++ project/tests.py
@@ -654,7 +654,19 @@ class testPassengerHomePage_integration_test_class(TestCase):
 
      response = self.client.get(('logoutUser'), follow=True)#log out
 
-     self.assertNotEqual(response.status_code, 300)            
+     self.assertNotEqual(response.status_code, 300)    
+
+   def test_PassengerPassword_ToPassengerNotification(self):
+     response = self.client.get('PassengerPassword')#url שלב 1
+     self.assertTrue(User.is_authenticated)
+
+     response = self.client.get(('PassengerNotification'))#url אחרי הפעולה
+     self.assertNotEqual(response.status_code, 300)
+
+     response = self.client.get(('logoutUser'), follow=True)#log out
+
+     self.assertNotEqual(response.status_code, 300)       
+        
 
 
 
@@ -665,16 +677,6 @@ class testPassengerHomePage_integration_test_class(TestCase):
 
 
 
-#   def test_PassengerPassword_ToPassengerNotification(self):
-#      response = self.client.get('PassengerPassword')#url שלב 1
-#      self.assertTrue(User.is_authenticated)
-
-#      response = self.client.get(('PassengerNotification'))#url אחרי הפעולה
-#      self.assertNotEqual(response.status_code, 300)
-
-#      response = self.client.get(('logoutUser'), follow=True)#log out
-
-#      self.assertNotEqual(response.status_code, 300)       
 
 
 #   def test_PassengerPassword_ToPMyTrip(self):
